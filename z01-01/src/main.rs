f9341bd1d08a6bcda9c37bd8e3aefa8ffa319ccb
Oskar Wiszowaty <ppwiszu@gmail.com>
2022-12-03 18:56:51 +0100

    Day 1 Task 2

--- z01-01/src/main.rs
+++ z01-01/src/main.rs
@@ -4,28 +4,33 @@ use std::path::Path;
 
 fn main() {
     if let Ok(lines) = read_lines("/home/wiszowaty/personal/CodeAdvent2022/z01-01/src/input") {
-        let mut max = 0;
+        let max: &mut [i32; 3] = &mut [0, 0, 0];
         let mut actual = 0;
 
         for line_res in lines {
             if let Ok(line) = line_res {
                 if line.len() == 0 {
-                    if actual > max {
-                        max = actual;
+                    if actual > max[0] {
+                        max[0] = actual;
+                        sort(max);
                     }
                     actual = 0;
                     continue;
                 }
-                let value : i32 = line.parse().unwrap();
+                let value: i32 = line.parse().unwrap();
                 actual += value;
             }
         }
 
-        if actual > max {
-            max = actual;
+        if max[0] < actual {
+            max[0] = actual;
+        } else if max[1] < actual {
+            max[1] = actual;
+        } else if max[2] < actual {
+            max[2] = actual;
         }
 
-        print!("{}", max.to_string());
+        print!("{}", (max[0] + max[1] + max[2]).to_string());
     }
 }
 
@@ -33,4 +38,16 @@ fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
 where P: AsRef<Path>, {
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
+}
+
+fn sort(input: &mut [i32; 3]) {
+    for i in 0..input.len() {
+        for j in 0..input.len() {
+            if input[i] < input[j] {
+                let tmp = input[i];
+                input[i] = input[j];
+                input[j] = tmp;
+            }
+        }
+    }
 }
